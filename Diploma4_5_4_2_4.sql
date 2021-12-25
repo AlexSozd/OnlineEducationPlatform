c93d8a90ca405cc599154b84fcc0ddf32e6e6c2e
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2021-12-25 21:57:38 +0300

    Add files via upload

---
+++ Diploma4_5_4_2_4.sql
@@ -0,0 +1,72 @@
+use Edaibd;
+
+/*
+CREATE TABLE dbo.TestBlocks
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_page INT NOT NULL FOREIGN KEY REFERENCES dbo.Pages(id),
+   name NVARCHAR(255) NOT NULL
+);
+CREATE TABLE dbo.TestQuestions
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   ques NVARCHAR(255) NOT NULL,
+   im_file NVARCHAR(255) NULL,
+   var1 NVARCHAR(255) NULL,
+   var2 NVARCHAR(255) NULL,
+   var3 NVARCHAR(255) NULL,
+   var4 NVARCHAR(255) NULL,
+   r_ans NVARCHAR(255) NOT NULL,
+   balls INT NOT NULL
+);
+CREATE TABLE dbo.Users
+(
+   id INT NOT NULL PRIMARY KEY,
+   name NVARCHAR(255) NOT NULL,
+   cell_tel NVARCHAR(255) NULL,
+   e_mail NVARCHAR(255) NULL,
+   parole NVARCHAR(255) NOT NULL
+);
+IF OBJECT_ID('dbo.ExamResults', 'U') IS NOT NULL
+   DROP TABLE dbo.ExamResults;
+CREATE TABLE dbo.ExamResults
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_us INT NOT NULL FOREIGN KEY REFERENCES dbo.Users(id),
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   real_balls REAL NOT NULL,
+   max_res INT NOT NULL,
+   procnt INT NOT NULL,
+   mark INT NOT NULL,
+   dt DATETIME NOT NULL,
+   trynum INT NOT NULL
+);
+IF OBJECT_ID('dbo.Answers', 'U') IS NOT NULL
+   DROP TABLE dbo.Answers;
+CREATE TABLE dbo.Answers
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_us INT NOT NULL FOREIGN KEY REFERENCES dbo.Users(id),
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   id_ex INT NOT NULL FOREIGN KEY REFERENCES dbo.ExamResults(id),
+   us_balls REAL NOT NULL
+);
+IF OBJECT_ID('dbo.PromptCount', 'U') IS NOT NULL
+   DROP TABLE dbo.PromptCount;
+CREATE TABLE dbo.PromptCount
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   max_num_pr INT NOT NULL
+);*/
+
+GO
+CREATE PROC FindUserName
+(
+  @id_us AS INT
+)
+AS
+  BEGIN
+    SELECT name FROM dbo.Users WHERE id=@id_us;
+  END;
