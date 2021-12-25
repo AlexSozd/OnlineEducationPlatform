d07d09b286badd2dd2d5a462291e3c018d1c1343
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2021-12-25 22:58:18 +0300

    Add files via upload

---
+++ Diploma_PersonalResults_1_1_1.sql
@@ -0,0 +1,128 @@
+use Edaibd;
+
+/*
+CREATE TABLE dbo.TestBlocks
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_page INT NOT NULL FOREIGN KEY REFERENCES dbo.Pages(id),
+   name NVARCHAR(255) NOT NULL
+);
+CREATE TABLE dbo.TestQuestions
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   ques NVARCHAR(255) NOT NULL,
+   im_file NVARCHAR(255) NULL,
+   var1 NVARCHAR(255) NULL,
+   var2 NVARCHAR(255) NULL,
+   var3 NVARCHAR(255) NULL,
+   var4 NVARCHAR(255) NULL,
+   r_ans NVARCHAR(255) NOT NULL,
+   balls INT NOT NULL
+);
+CREATE TABLE dbo.Users
+(
+   id INT NOT NULL PRIMARY KEY,
+   name NVARCHAR(255) NOT NULL,
+   cell_tel NVARCHAR(255) NULL,
+   e_mail NVARCHAR(255) NULL,
+   parole NVARCHAR(255) NOT NULL
+);
+IF OBJECT_ID('dbo.ExamResults', 'U') IS NOT NULL
+   DROP TABLE dbo.ExamResults;
+CREATE TABLE dbo.ExamResults
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_us INT NOT NULL FOREIGN KEY REFERENCES dbo.Users(id),
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   real_balls REAL NOT NULL,
+   max_res INT NOT NULL,
+   procnt INT NOT NULL,
+   mark INT NOT NULL,
+   dt DATETIME NOT NULL,
+   trynum INT NOT NULL
+);
+IF OBJECT_ID('dbo.Answers', 'U') IS NOT NULL
+   DROP TABLE dbo.Answers;
+CREATE TABLE dbo.Answers
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_us INT NOT NULL FOREIGN KEY REFERENCES dbo.Users(id),
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   id_ex INT NOT NULL FOREIGN KEY REFERENCES dbo.ExamResults(id),
+   us_balls REAL NOT NULL
+);
+IF OBJECT_ID('dbo.PromptCount', 'U') IS NOT NULL
+   DROP TABLE dbo.PromptCount;
+CREATE TABLE dbo.PromptCount
+(
+   id INT NOT NULL PRIMARY KEY,
+   id_bl INT NOT NULL FOREIGN KEY REFERENCES dbo.TestBlocks(id),
+   max_num_pr INT NOT NULL
+);*/
+
+GO
+CREATE PROC ShowUserAnswersInTest
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname);
+  END;
+
+GO
+CREATE PROC ShowUserResults2
+(
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname);
+  END;
+
+GO
+CREATE PROC ShowUserAnswersInTestForTimePeriod
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2);
+  END;
+
+GO
+CREATE PROC ShowUserResultsInTimePeriod
+(
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2);
+  END;
+
+GO
+CREATE PROC PersonalTestAnswers
+(
+  @id_ex AS INT
+)
+AS
+  BEGIN
+    SELECT TestQuestions.ques, Answers.us_balls, TestQuestions.balls, Answers.us_ans, Answers.tr_ans FROM (dbo.Answers JOIN dbo.TestQuestions ON Answers.id_bl=TestQuestions.id_bl) WHERE id_ex=@id_ex;
+  END;
+
+GO
+CREATE PROC ShowTestTaskList
+(
+  @name AS NVARCHAR(255)
+)
+AS
+  BEGIN
+    SELECT ques FROM dbo.TestQuestions WHERE id_bl=(SELECT id_bl FROM dbo.TestBlocks WHERE name=@name);
+  END;
---
+++ Diploma_SpecialQueries.sql
@@ -0,0 +1,174 @@
+use Edaibd;
+
+GO
+CREATE PROC ShowBestResultsByTest
+(
+  @tname AS NVARCHAR(255)
+)
+AS
+  BEGIN
+    SELECT ExamResults.id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND
+	procnt=(SELECT MAX(procnt) FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) GROUP BY Users.id);
+  END;
+GO
+CREATE PROC ShowLastResultsByTest
+(
+  @tname AS NVARCHAR(255)
+)
+AS
+  BEGIN
+    SELECT ExamResults.id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND
+	dt=(SELECT MAX(dt) FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) GROUP BY Users.id);
+  END;
+GO
+CREATE PROC ShowBestUserAnswerInTest
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND
+	procnt=(SELECT MAX(procnt) FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname));
+  END;
+GO
+CREATE PROC ShowLastUserAnswerInTest
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND
+	dt=(SELECT MAX(dt) FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname));
+  END;
+GO
+CREATE PROC ShowBestUserResults
+(
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND
+	procnt=(SELECT MAX(procnt) FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) GROUP BY TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowLastUserResults
+(
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND
+	dt=(SELECT MAX(dt) FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) GROUP BY TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowAllBestResults
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, Users.name AS UserName, TestBlocks.name AS TestName, mark, real_balls, max_res, dt FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE
+	procnt=(SELECT MAX(procnt) FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) GROUP BY Users.id, TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowAllLastResults
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, Users.name AS UserName, TestBlocks.name AS TestName, mark, real_balls, max_res, dt FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE
+	dt=(SELECT MAX(dt) FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) GROUP BY Users.id, TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowBestResultsByTestForPeriod
+(
+  @tname AS NVARCHAR(255),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	procnt=(SELECT MAX(procnt) FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND (dt>=@dt1) AND (dt<=@dt2) GROUP BY Users.id);
+  END;
+GO
+CREATE PROC ShowLastResultsByTestForPeriod
+(
+  @tname AS NVARCHAR(255),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	dt=(SELECT MAX(dt) FROM (dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) WHERE id_bl=(SELECT id FROM dbo.TestBlocks WHERE name=@tname) AND (dt>=@dt1) AND (dt<=@dt2) GROUP BY Users.id);
+  END;
+GO
+CREATE PROC ShowBestUserAnswerInTestForPeriod
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	procnt=(SELECT MAX(procnt) FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2));
+  END;
+GO
+CREATE PROC ShowLastUserAnswerInTestForPeriod
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	dt=(SELECT MAX(dt) FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2));
+  END;
+GO
+CREATE PROC ShowBestUserResultsForPeriod
+(
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	procnt=(SELECT MAX(procnt) FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) GROUP BY TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowLastUserResultsForPeriod
+(
+  @usname AS NVARCHAR(100),
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, name, dt, real_balls, max_res, procnt, mark FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) AND
+	dt=(SELECT MAX(dt) FROM (dbo.ExamResults JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE id_us=(SELECT id FROM dbo.Users WHERE name=@usname) AND (dt>=@dt1) AND (dt<=@dt2) GROUP BY TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowAllBestResultsForPeriod
+(
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, Users.name AS UserName, TestBlocks.name AS TestName, mark, real_balls, max_res, dt FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE (dt>=@dt1) AND (dt<=@dt2) AND
+	procnt=(SELECT MAX(procnt) FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE (dt>=@dt1) AND (dt<=@dt2) GROUP BY Users.id, TestBlocks.id);
+  END;
+GO
+CREATE PROC ShowAllLastResultsForPeriod
+(
+  @dt1 AS DATE,
+  @dt2 AS DATE
+)
+AS
+  BEGIN
+    SELECT ExamResults.id AS id, Users.name AS UserName, TestBlocks.name AS TestName, mark, real_balls, max_res, dt FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE (dt>=@dt1) AND (dt<=@dt2) AND
+	dt=(SELECT MAX(dt) FROM ((dbo.ExamResults JOIN dbo.Users ON ExamResults.id_us=Users.id) JOIN dbo.TestBlocks ON ExamResults.id_bl=TestBlocks.id) WHERE (dt>=@dt1) AND (dt<=@dt2) GROUP BY Users.id, TestBlocks.id);
+  END;
