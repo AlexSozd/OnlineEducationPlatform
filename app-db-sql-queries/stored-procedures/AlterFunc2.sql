1094bc725e0c72a8c0bd35c71e248bbfe581c822
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2022-01-05 22:06:31 +0300

    SQL queries for stored procedures correction
    User result representation

---
+++ app-db-sql-queries/stored-procedures/AlterFunc2.sql
@@ -0,0 +1,16 @@
+﻿USE [Edaibd]
+GO
+/****** Object:  StoredProcedure [dbo].[ShowUserAnswersInTest]    Script Date: 13.05.2019 19:55:23 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROC [dbo].[ShowUserAnswersInTest]
+(
+  @tname AS NVARCHAR(255),
+  @usname AS NVARCHAR(100)
+)
+AS
+  BEGIN
+    SELECT id, mark, real_balls, max_res, procnt, dt FROM dbo.ExamResults WHERE id_bl=(SELECT id FROM TestBlocks WHERE name=@tname) AND id_us=(SELECT id FROM Users WHERE name=@usname);
+  END;
