dd139af7c4723509d98f2ee4390f44a83231ecc8
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2022-01-17 17:01:07 +0300

    Stored procedures creation (educational content)

--- app-db-sql-queries/stored-procedures/Diploma2_1.sql
+++ app-db-sql-queries/stored-procedures/Diploma2_1.sql
@@ -1,24 +1,23 @@
 use Edaibd;
-go
+GO
 CREATE PROC SetLevels
 AS
   BEGIN
-  SELECT * FROM Levels;
+    SELECT * FROM Levels;
   END;
-  GO
+GO
 CREATE PROC SetSections
 AS
   BEGIN
-  SELECT * FROM Sections;
+    SELECT * FROM Sections;
   END;
-  GO
+GO
 CREATE PROC SetLessons
 AS
   BEGIN
-  SELECT * FROM Lessons;
+    SELECT * FROM Lessons;
   END;
-  GO
-
+GO
 CREATE PROC ShowSections
 (
   @id_sec AS INT,
@@ -26,45 +25,43 @@ CREATE PROC ShowSections
 )
 AS
   BEGIN
-  SELECT id, name FROM Sections WHERE id_up=@id_sec AND id_lev=@lev;
+    SELECT id, name FROM Sections WHERE id_up=@id_sec AND id_lev=@lev;
   END;
-  GO
+GO
 CREATE PROC ShowFirstLevel
 AS
   BEGIN
-  SELECT id, name FROM Sections WHERE id_lev=1;
+    SELECT id, name FROM Sections WHERE id_lev=1;
   END;
-  GO
+GO
 CREATE PROC ShowLessons
 (
   @id_sec AS INT
 )
 AS
   BEGIN
-  SELECT id, name FROM Lessons WHERE id_sec=@id_sec;
+    SELECT id, name FROM Lessons WHERE id_sec=@id_sec;
   END;
-  GO
+GO
 CREATE PROC GetLessonPage
 (
   @id_lesson AS INT
 )
 AS
   BEGIN
-  SELECT * FROM Pages WHERE id_les=@id_lesson;
+    SELECT * FROM Pages WHERE id_les=@id_lesson;
   END;
-  GO
-
+GO
 CREATE PROC GetPageLesson
 (
   @id_page AS INT
 )
 AS
   BEGIN
-  --SELECT * FROM Pages WHERE id_les=@id_lesson;
-  SELECT id, name FROM Lessons WHERE id=(SELECT id_les FROM Pages WHERE id=@id_page);
+    --SELECT * FROM Pages WHERE id_les=@id_lesson;
+    SELECT id, name FROM Lessons WHERE id=(SELECT id_les FROM Pages WHERE id=@id_page);
   END;
-  GO
-
+GO
 CREATE FUNCTION dbo.GetNumFirPage
 (
   @id_lesson AS INT
@@ -72,11 +69,12 @@ CREATE FUNCTION dbo.GetNumFirPage
 RETURNS INT
 AS
   BEGIN
-  DECLARE @i AS INT
-  SET @i = (SELECT MIN(id) FROM Pages WHERE id_les=@id_lesson);
-  RETURN @i
+    DECLARE @i AS INT
+    SET @i = (SELECT MIN(id) FROM Pages WHERE id_les=@id_lesson);
+    RETURN @i
   END;
-  GO
+
+GO
 /*CREATE FUNCTION dbo.GetNumFolPage
 (
   @id_page AS INT
@@ -84,48 +82,49 @@ AS
 RETURNS INT
 AS
   BEGIN
-  DECLARE @i AS INT
-  SET @i = (SELECT id FROM Pages WHERE id=@id_page);
-  RETURN @i
+    DECLARE @i AS INT
+    SET @i = (SELECT id FROM Pages WHERE id=@id_page);
+    RETURN @i
   END;
-  GO*/
+GO*/
+
 CREATE PROC GetLesson
 (
   @id_lesson AS INT
 )
 AS
   BEGIN
-  SELECT * FROM Lessons WHERE id=@id_lesson;
+    SELECT * FROM Lessons WHERE id=@id_lesson;
   END;
-  GO
+GO
 CREATE PROC GetPage
 (
   @id_page AS INT
 )
 AS
   BEGIN
-  SELECT * FROM Pages WHERE id=@id_page;
+    SELECT * FROM Pages WHERE id=@id_page;
   END;
-  GO
+GO
 CREATE PROC GetTerm
 (
   @id_term AS INT
 )
 AS
   BEGIN
-  SELECT * FROM Terms WHERE id=@id_term;
+    SELECT * FROM Terms WHERE id=@id_term;
   END;
-  GO
+GO
 CREATE PROC SetPageRefs
 (
   @id_page AS INT
 )
 AS
   BEGIN
-  --SELECT id, name FROM (SELECT * FROM PageRefs JOIN Terms on Terms.id=id_term) AS T WHERE id_page=@id_page;
-  SELECT id, name FROM Terms WHERE id IN (SELECT id_term FROM PageRefs WHERE id_page=@id_page); 
+    --SELECT id, name FROM (SELECT * FROM PageRefs JOIN Terms on Terms.id=id_term) AS T WHERE id_page=@id_page;
+    SELECT id, name FROM Terms WHERE id IN (SELECT id_term FROM PageRefs WHERE id_page=@id_page); 
   END;
-  GO
+GO
 
 /*CREATE PROC SetPageRefs
 (
@@ -133,9 +132,9 @@ AS
 )
 AS
   BEGIN
-  SELECT * FROM PageRefs WHERE id_page=@id_page;
+    SELECT * FROM PageRefs WHERE id_page=@id_page;
   END;
-  GO*/
+GO*/
 
 CREATE PROC SetInRefs
 (
@@ -143,19 +142,20 @@ CREATE PROC SetInRefs
 )
 AS
   BEGIN
-  SELECT id, name FROM Terms WHERE id IN (SELECT id_term FROM InRefs WHERE id_cur=@id_cur);
+    SELECT id, name FROM Terms WHERE id IN (SELECT id_term FROM InRefs WHERE id_cur=@id_cur);
   END;
-  GO
 
+GO
 /*CREATE PROC SetInRefs
 (
   @id_cur AS INT
 )
 AS
   BEGIN
-  SELECT * FROM InRefs WHERE id_cur=@id_cur;
+    SELECT * FROM InRefs WHERE id_cur=@id_cur;
   END;
-  GO*/
+GO*/
+
 CREATE FUNCTION PagesCount
 (
 
@@ -163,8 +163,8 @@ CREATE FUNCTION PagesCount
 RETURNS INT
 AS
   BEGIN
-  DECLARE @i AS INT
-  SET @i = (SELECT COUNT(*) FROM Pages);
-  RETURN @i
+    DECLARE @i AS INT
+    SET @i = (SELECT COUNT(*) FROM Pages);
+    RETURN @i
   END;
-  GO
+GO
